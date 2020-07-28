43b179435775924f4b7a5c8c58eb7c7d046fbeab
Ammon Roberts <ammoneroberts@gmail.com>
2020-07-27 19:59:25 -0600

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -5,7 +5,7 @@ import csv
 
 # data = {}
 
-with open('src/data/test1.csv', newline='') as csvfile:
+with open('data/test1.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
     print(type(reader))
     for row in reader:
