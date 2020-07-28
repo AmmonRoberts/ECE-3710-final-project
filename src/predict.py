72e0f8a6f6979e6551995e918deabf025bf1a28d
Ammon Roberts <ammoneroberts@gmail.com>
2020-07-27 21:07:29 -0600

    Update all

--- src/predict.py
+++ src/predict.py
@@ -3,10 +3,81 @@ import numpy as n
 import matplotlib as m
 import csv
 
-# data = {}
+global fg
+global threePt
+global margin
+fg = []
+threePt = []
+margin = []
 
-with open('data/Jazz5.csv', newline='', encoding='utf8') as csvfile:
+confidence = 0.95
+n = 5
+
+# m = mean(data)
+# std_err = sem(data)
+# h = std_err * t.ppf((1 + confidence) / 2, n - 1)
+# start = m - h
+
+
+def calculateConfidence(input):
+    m = s.mean(data)
+
+
+def getData(r):
+    fg.append(r['FG %'])
+    threePt.append(r['3pt %'])
+    margin.append(r['MofV'])
+
+
+def clearLists():
+    print('heck')
+    fg = []
+    threePt = []
+    margin = []
+    print(margin)
+
+
+with open('data/UTA5.csv', newline='', encoding='utf8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-    print(type(reader))
+
     for row in reader:
-        print(row)
+        getData(row)
+        # print(row['FG %'])
+    print(margin)
+    clearLists()
+    print(margin)
+
+
+# with open('data/SAC5.csv', newline='', encoding='utf8') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+
+#     for row in reader:
+#         print(row)
+
+
+# with open('data/MIL5.csv', newline='', encoding='utf8') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+
+#     for row in reader:
+#         print(row)
+
+
+# with open('data/PHI5.csv', newline='', encoding='utf8') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+
+#     for row in reader:
+#         print(row)
+
+
+# with open('data/GSW5.csv', newline='', encoding='utf8') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+
+#     for row in reader:
+#         print(row)
+
+
+# with open('data/LAC5.csv', newline='', encoding='utf8') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+
+#     for row in reader:
+#         print(row)
