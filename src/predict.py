23bfaff780d4a2590066143a379d7f0cb2d9238f
Ammon Roberts <ammoneroberts@gmail.com>
2020-08-02 23:00:27 -0600

    Update predict.py
    Added a few more functions, which cut the script in half line-wise.
    Updated functions to follow naming conventions.

--- src/predict.py
+++ src/predict.py
@@ -2,13 +2,15 @@ import scipy as s
 import numpy as n
 import matplotlib as m
 import csv
+import io
+import sys
 import warnings
 import scipy.stats
 
 # Ignore deprecation warnings
 warnings.filterwarnings("ignore")
 
-# data object containing lists for each major stat category
+# Dictionary of lists containing lists for each major stat category
 data = {'fg': [], 'threePt': [], 'margin': []}
 
 # Declare some constants: confidence level, array size (number of games)
@@ -18,7 +20,7 @@ n = 5
 # Calculates the interval based on the array that was given
 
 
-def calculateConfidence(input):
+def calculate_confidence(input):
     m = s.mean(input)
     std_err = s.stats.sem(input)
     h = std_err * s.stats.t.ppf((1 + confidence) / 2, n - 1)
@@ -30,113 +32,45 @@ def calculateConfidence(input):
 # CSV and adds it to its corresponding list
 
 
-def getData(r):
+def get_data(r):
 
     data['fg'].append(float(r['FG%']))
     data['threePt'].append(float(r['3pt%']))
     data['margin'].append(float(r['MofV']))
 
 
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+def load_csv_calculate_and_print(filename):
+    global data
+    print(f'{filename}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    disable_print()
+    with open(f'data/{filename}', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
 
-print('UTA\n')
-with open('data/UTA5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            get_data(row)
+            print(row)
 
-    for row in reader:
-        getData(row)
-        print(row)
+        enable_print()
+        print(f'Field Goal Confidence Interval:')
+        calculate_confidence(data["fg"])
+        print(f'Three Point Confidence Interval:')
+        calculate_confidence(data["threePt"])
+        print(f'Margin of Victory Confidence Interval:')
+        calculate_confidence(data["margin"])
+    data = {'fg': [], 'threePt': [], 'margin': []}
 
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
 
-data = {'fg': [], 'threePt': [], 'margin': []}
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+def disable_print():
+    text_trap = io.StringIO()
+    sys.stdout = text_trap
 
-print('SAC\n')
-with open('data/SAC5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
 
-    for row in reader:
-        getData(row)
+def enable_print():
+    sys.stdout = sys.__stdout__
 
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
 
-data = {'fg': [], 'threePt': [], 'margin': []}
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-
-print('MIL\n')
-with open('data/MIL5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-
-    for row in reader:
-        getData(row)
-
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
-
-data = {'fg': [], 'threePt': [], 'margin': []}
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-
-print('PHI\n')
-with open('data/PHI5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-
-    for row in reader:
-        getData(row)
-
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
-
-data = {'fg': [], 'threePt': [], 'margin': []}
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-
-print('GSW\n')
-with open('data/GSW5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-
-    for row in reader:
-        getData(row)
-
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
-
-data = {'fg': [], 'threePt': [], 'margin': []}
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-
-print('LAC\n')
-with open('data/LAC5.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-
-    for row in reader:
-        getData(row)
-
-    print(f'Field Goal Confidence Interval:')
-    calculateConfidence(data["fg"])
-    print(f'Three Point Confidence Interval:')
-    calculateConfidence(data["threePt"])
-    print(f'Margin of Victory Confidence Interval:')
-    calculateConfidence(data["margin"])
-
-data = {'fg': [], 'threePt': [], 'margin': []}
+load_csv_calculate_and_print("UTA5.csv")
+load_csv_calculate_and_print("GSW5.csv")
+load_csv_calculate_and_print("UTA5.csv")
+load_csv_calculate_and_print("GSW5.csv")
+load_csv_calculate_and_print("GSW5.csv")
