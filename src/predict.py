09194a20e9e1dc17bf9e12e2798fc1165d0af522
cpaulson09 <cpaulson09@hotmail.com>
2020-08-11 03:29:22 -0600

    added function for comparing stats

--- src/predict.py
+++ src/predict.py
@@ -7,24 +7,29 @@ import sys
 import warnings
 import scipy.stats
 
+# Declare some constants: confidence level, array size (number of games)
+confidence = 0.95
+numGames = 5
+# Counters
+win = 0
+loss = 0
+
 # Ignore deprecation warnings
 warnings.filterwarnings("ignore")
 
 # Dictionary of lists containing lists for each major stat category
 data = {'fg': [], 'threePt': [], 'margin': []}
-uta_means = {'fg': -1, 'threePt': -1, 'margin': -1}
+utahJazz_means = {'fg': -1, 'threePt': -1, 'margin': -1}
+otherTeam_means = {'fg': -1, 'threePt': -1, 'margin': -1}
+means = {}
+
 
 # Create dummy list for other teams, then use them to compare to the uta means
 # Add to each counter
 # Clear out the dummy list
 # Use MoV as a tie-breaker between win/loss
 
-win = 0
-loss = 0
 
-# Declare some constants: confidence level, array size (number of games)
-confidence = 0.95
-numGames = 5
 
 
 # Calculates the interval based on the array that was given
@@ -37,17 +42,50 @@ def calculate_confidence(input):
     print(f'Start: {start}\nEnd: {end}\n\n')
 
 
-def calculate_mean(input, filename):
-    m = s.mean(input)
-    f = filename.split('5')[0]
-    means[f'{f}'] = m
-    print(f'Mean: {means[f]}\n\n')
+def calculate_mean(input, filename, field):
+    mean = s.mean(input)
+    team = filename.split('5')[0]
+    means[f'{team}'] = mean
+    print(f'Mean: {means[team]}')
+    if team == 'UTA':
+        if field == 'fg':
+            utahJazz_means['fg'] = mean
+        elif field == '3pt':
+            utahJazz_means['threePt'] = mean
+        elif field == 'margin': 
+            utahJazz_means['margin'] = mean
+        print(utahJazz_means)
+
+    # other team
+    else:
+        if field == 'fg':
+            otherTeam_means['fg'] = mean
+        elif field == '3pt':
+            otherTeam_means['threePt'] = mean
+        elif field == 'margin': 
+            otherTeam_means['margin'] = mean
+        print(otherTeam_means)
+
+
+# Take and compare sample mean data for jazz and other 
+# team and determine winner, MofV being the tie breaker
+def predict_wins(utahJazz, otherTeam):
+    # if jazz are better at both, we predict a win
+    if utahJazz['fg'] > otherTeam['fg']:
+        if utahJazz['threePt'] > otherTeam['threePt']:
+            win += 1
+    # if jazz are worse at both, we predict a loss
+    elif utahJazz['fg'] < otherTeam['fg']:
+        if utahJazz['threePt'] < otherTeam['threePt']:
+            loss += 1
+    # tie-breaker
+    elif utahJazz['margin'] > otherTeam['margin']:
+        win += 1
+        else loss += 1
 
 
 # Grabs each of these fields from each row of the
 # CSV and adds it to its corresponding list
-
-
 def get_data(r):
 
     data['fg'].append(float(r['FG%']))
@@ -59,7 +97,7 @@ def get_data(r):
 # Then calls the calculate_confidence function to calculate the interval
 def load_csv_calculate_and_print(filename):
     global data
-    print(f'{filename}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    print(f'{filename}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
     disable_print()
     with open(f'data/{filename}', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
@@ -79,20 +117,20 @@ def load_csv_calculate_and_print(filename):
         # calculate_confidence(data["margin"])
 
         print(f'Field Goal Mean:')
-        calculate_mean(data["fg"], filename)
+        calculate_mean(data["fg"], filename, 'fg')
         print(f'Three Point Mean:')
-        calculate_mean(data["threePt"], filename)
+        calculate_mean(data["threePt"], filename, '3pt')
         print(f'Margin of Victory Mean:')
-        calculate_mean(data["margin"], filename)
+        calculate_mean(data["margin"], filename, 'margin')
 
     data = {'fg': [], 'threePt': [], 'margin': []}
+    print('\n\n')
 
 
 # Function for disabling stdout, so that calling load_csv_calculate_and_print doesn't print out the contents of the CSV
 def disable_print():
     text_trap = io.StringIO()
     sys.stdout = text_trap
-
 
 # Enables stdout again, so data can be displayed
 def enable_print():
