92630964971d701d4c461daa85d4806654c4595f
Ammon Roberts <ammoneroberts@gmail.com>
2020-08-11 20:09:47 -0600

    Update all
    Fixed and unified some formatting
    Fixed an issue with not reading all CSVs
    Added and fixed items in the readme

--- src/README.txt
+++ src/README.txt
@@ -1,13 +1,37 @@
 You must have Python 3.8 installed on your computer in order to run this script.
-
 For instructions on how to accomplish this, visit https://www.python.org/.
-
 For instructions installing pip, visit https://pip.pypa.io/en/stable/installing/.
-
 To install the needed libraries, run "python -m pip install --user numpy scipy matplotlib" from the command line.
 
-To run the script, open a Command Window or terminal in the src folder, then run "python predict.py" or "python3 predict.py", depending on what version of Python your Python path variable points to. 
+How to run the script:
+1. Open a Command Window or terminal in the src folder
+2. Run "python predict.py" or "python3 predict.py", depending on what version of Python your Python path variable points to. 
 
-This Python script automatically imports the raw data from the src/data folder. The CSV files MUST be located in this folder, otherwise the script will not work. The CSV files must also be saved without encoding options, as the script does not support encoding.
-
+This Python script automatically imports the raw data from the src/data folder. 
+The CSV files MUST be located in this folder, otherwise the script will not work. 
+The CSV files must also be saved without encoding options, as the script does not support encoding.
 The script currently only supports a dataset of 5 items, but this can be changed if n is set equal to the number of tiems in a sample or dataset. 
+
+This script can also be further changed to work for data from other teams.
+
+This can be done by doing the following:
+1. Obtain the game data for a team of your choosing from https://www.basketball-reference.com/
+2. Copy the data for field goals, three pointers, and margin of vitory into a CSV, following the format of the included CSVs (including headers)
+3. Edit the following lines of code (lines 145 - 149) and use the names of the CSVs you created:
+    `
+    load_csv_calculate_and_print("TEAM.csv")
+    load_csv_calculate_and_print("FIRSTOPPONENT.csv")
+    load_csv_calculate_and_print("SECONDOPPONENT.csv")
+    load_csv_calculate_and_print("THIRDOPPONENT.csv")
+    load_csv_calculate_and_print("FOURTHOPPONENT.csv")
+    load_csv_calculate_and_print("FIFTHOPPONENT.csv")
+    `
+4. Create another CSV using the actual game data for the next 5 games, following the format of ACTUAL.CSV
+    You must track a win or loss using the characters 'W' or 'L', respectively
+5. (NOT FULLY SUPPORTED) If you use the data for more than 5 games, you will need to update the following line of code (line 15) accordingly:
+    `
+    num_games = n
+    `
+    Where n is the number of games used in the sample
+
+6. Run the script using the above instructions
--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,5 @@
 import scipy as s
-import numpy as num
+# import numpy as num
 # import matplotlib as m
 import csv
 import io
@@ -7,21 +7,21 @@ import sys
 import warnings
 import scipy.stats
 
-# Ignore deprecation warnings
+# Ignore package deprecation warnings
 warnings.filterwarnings("ignore")
 
 # Declare some constants: confidence level, array size (number of games), W/L counters
 confidence = 0.95
-numGames = 5
+num_games = 5
 win = 0
 loss = 0
-actualWin = 0
-actualLoss = 0
+actual_Win = 0
+actual_Loss = 0
 
 # Dictionary of lists containing lists for each major stat category
 data = {'fg': [], 'threePt': [], 'margin': []}
-utahJazz_means = {'fg': -1, 'threePt': -1, 'margin': -1}
-otherTeam_means = {'fg': -1, 'threePt': -1, 'margin': -1}
+utah_jazz_means = {'fg': -1, 'threePt': -1, 'margin': -1}
+other_team_means = {'fg': -1, 'threePt': -1, 'margin': -1}
 means = {}
 
 
@@ -29,15 +29,16 @@ means = {}
 def calculate_confidence(input):
     m = s.mean(input)
     std_err = s.stats.sem(input)
-    h = std_err * s.stats.t.ppf((1 + confidence) / 2, numGames - 1)
+    h = std_err * s.stats.t.ppf((1 + confidence) / 2, num_games - 1)
     start = m - h
     end = m + h
     print(f'Start: {start}\nEnd: {end}\n\n')
 
 
+# Calculates the mean scores for the Jazz as well as the 5 other teams
 def calculate_mean(input, filename, field):
-    global utahJazz_means
-    global otherTeam_means
+    global utah_jazz_means
+    global other_team_means
 
     mean = s.mean(input)
     team = filename.split('5')[0]
@@ -45,53 +46,48 @@ def calculate_mean(input, filename, field):
     print(f'Mean: {means[team]}')
     if team == 'UTA':
         if field == 'fg':
-            utahJazz_means['fg'] = mean
+            utah_jazz_means['fg'] = mean
         elif field == '3pt':
-            utahJazz_means['threePt'] = mean
+            utah_jazz_means['threePt'] = mean
         elif field == 'margin':
-            utahJazz_means['margin'] = mean
+            utah_jazz_means['margin'] = mean
 
-    # other team
     else:
         if field == 'fg':
-            otherTeam_means['fg'] = mean
+            other_team_means['fg'] = mean
         elif field == '3pt':
-            otherTeam_means['threePt'] = mean
+            other_team_means['threePt'] = mean
         elif field == 'margin':
-            otherTeam_means['margin'] = mean
-
-    # compare_stats(utahJazz_means, otherTeam_means)
+            other_team_means['margin'] = mean
 
 
 # Take and compare sample mean data for jazz and other
 # team and determine winner, MofV being the tie breaker
-def compare_stats(utahJazz, otherTeam):
+def compare_stats(utah_jazz, other_team):
     global win
     global loss
-
-    # if jazz are better at both, we predict a win
-    if utahJazz['fg'] > otherTeam['fg'] and utahJazz['threePt'] > otherTeam['threePt']:
-        win += 1
-        print('\nJazz win\n')
-    # if jazz are worse at both, we predict a loss
-    elif utahJazz['fg'] < otherTeam['fg'] and utahJazz['threePt'] < otherTeam['threePt']:
-        loss += 1
-        print('\nJazz lose\n')
-    # tie-breaker
-    else:
-        if utahJazz['margin'] > otherTeam['margin']:
+    if other_team_means != ({'fg': -1, 'threePt': -1, 'margin': -1}):
+        # If Jazz are better at both, we predict a win
+        if utah_jazz['fg'] > other_team['fg'] and utah_jazz['threePt'] > other_team['threePt']:
             win += 1
             print('\nJazz win\n')
-        else:
+        # If Jazz are worse at both, we predict a loss
+        elif utah_jazz['fg'] < other_team['fg'] and utah_jazz['threePt'] < other_team['threePt']:
             loss += 1
             print('\nJazz lose\n')
-            
+        # Tie-breaker
+        else:
+            if utah_jazz['margin'] > other_team['margin']:
+                win += 1
+                print('\nJazz win\n')
+            else:
+                loss += 1
+                print('\nJazz lose\n')
 
 
 # Grabs each of these fields from each row of the
 # CSV and adds it to its corresponding list
 def get_data(r):
-
     data['fg'].append(float(r['FG%']))
     data['threePt'].append(float(r['3pt%']))
     data['margin'].append(float(r['MofV']))
@@ -101,8 +97,8 @@ def get_data(r):
 # Then calls the calculate_confidence function to calculate the interval
 def load_csv_calculate_and_print(filename):
     global data
-    global utahJazz_means
-    global otherTeam_means
+    global utah_jazz_means
+    global other_team_means
 
     print(f'{filename}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
     with open(f'data/{filename}', newline='') as csvfile:
@@ -126,21 +122,24 @@ def load_csv_calculate_and_print(filename):
         print(f'Margin of Victory Mean:')
         calculate_mean(data["margin"], filename, 'margin')
 
-    compare_stats(utahJazz_means, otherTeam_means)
-    data = {'fg': [], 'threePt': [], 'margin': []}
+    if other_team_means != ({'fg': -1, 'threePt': -1, 'margin': -1}):
+        compare_stats(utah_jazz_means, other_team_means)
+        data = {'fg': [], 'threePt': [], 'margin': []}
     print('\n')
 
 
 # Function for disabling stdout, so that calling load_csv_calculate_and_print doesn't print out the contents of the CSV
+# Didn't end up needing
 def disable_print():
     text_trap = io.StringIO()
     sys.stdout = text_trap
 
+
 # Enables stdout again, so data can be displayed
-
-
+# Didn't end up needing
 def enable_print():
     sys.stdout = sys.__stdout__
+
 
 print('\n')
 load_csv_calculate_and_print("UTA5.csv")
@@ -148,14 +147,18 @@ load_csv_calculate_and_print("LAC5.csv")
 load_csv_calculate_and_print("MIL5.csv")
 load_csv_calculate_and_print("SAC5.csv")
 load_csv_calculate_and_print("GSW5.csv")
+load_csv_calculate_and_print("PHI5.csv")
 
 print(f'Jazz predicted record over their next 5 games (W/L): {win}-{loss}\n')
 
+
+# Open and parse the actual game stats
 with open(f'data/ACTUAL.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
-        if row['result'] == 'W':
-            actualWin += 1
-        elif row['result'] == 'L':
-            actualLoss += 1
-print(f'Jazz actual record over their next 5 games (W/L): {actualWin}-{actualLoss}\n')
+        if row['Result'] == 'W':
+            actual_Win += 1
+        elif row['Result'] == 'L':
+            actual_Loss += 1
+print(
+    f'Jazz actual record over their next 5 games (W/L): {actual_Win}-{actual_Loss}\n')
